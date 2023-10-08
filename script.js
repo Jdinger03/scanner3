a3a2418b5671241c09bc5d059073a7a1ee2ba691
Jdinger03 <50883801+Jdinger03@users.noreply.github.com>
2023-10-08 03:54:28 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const barcodeScanner = new BarcodeScanner({
-  videoElement: document.getElementById('barcode-scanner'),
-});
-
-barcodeScanner.on('barcodeDetected', (barcode) => {
-  document.getElementById('barcode-data').innerHTML = barcode.data;
-});
